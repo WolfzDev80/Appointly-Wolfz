ef8b83870804c5d24bbabed51101edb32a665a4b
WolfzDev80 <wolfzdev80@gmail.com>
2026-03-02 21:50:21 -0500

    Update route.ts

--- appointly/src/app/api/appointments/[id]/route.ts
+++ appointly/src/app/api/appointments/[id]/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { NextRequest } from 'next/server'
 import { updateAppointmentSchema } from '@/lib/validations'
 import { getAppointmentById, updateAppointment, deleteAppointment } from '@/services/appointments'
