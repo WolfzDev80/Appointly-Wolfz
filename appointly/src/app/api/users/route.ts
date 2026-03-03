9e84cc34eff7a1c2a207ca1133e06fa7f2d4fd6f
WolfzDev80 <wolfzdev80@gmail.com>
2026-03-02 21:53:12 -0500

    Update route.ts

--- appointly/src/app/api/users/route.ts
+++ appointly/src/app/api/users/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic'
 import { NextRequest } from 'next/server'
 import { getAllUsers, getCurrentUser } from '@/services/users'
 import { createApiError, createApiSuccess } from '@/lib/utils'
